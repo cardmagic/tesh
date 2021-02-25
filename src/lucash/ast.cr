1ff45d44c396a7d1424b36136c58e87204d21381
Lucas Carlson <lucas@carlson.net>
2021-02-25 10:23:56 -0800

    Adding ast and parser

---
+++ src/lucash/ast.cr
@@ -0,0 +1,32 @@
+require "./token"
+
+module Lucash
+  class Node
+    def initialize(@token)
+    end
+
+    def token_literal
+    end
+  end
+
+  class Statement
+    def initialize(@node)
+    end
+
+    def statement_node
+    end
+  end
+
+  class Expression
+    def initialize(@node)
+    end
+
+    def expression_node
+    end
+  end
+
+  class Program
+    def initialize(@statements)
+    end
+  end
+end
--- src/lucash/parser.cr
+++ src/lucash/parser.cr
@@ -1,7 +1,25 @@
+require "./ast"
+require "./lexer"
+require "./token"
+
 module Lucash
   class ParseError < Exception; end
 
   class Parser
+    def initialize(@lexer)
+      # Read two token, so @cur_token and @peek_token are both set
+      next_token
+      next_token
+    end
+
+    def next_token
+      @cur_token = @peek_token
+      @peek_token = @lexer.next_token
+    end
+
+    def parse_program
+    end
+
     def parse(string)
       raise ParseError.new("Nothing works yet")
     end
